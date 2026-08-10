ca546aa456a0088a9b9720358037f26dcb68d7a5
Greg Hazenfield <gregh@hgsolutionswi.com>
2026-08-10 09:33:44 -0500

    Protect My Blueprints behind authentication

---
+++ app/blueprint/projects/layout.tsx
@@ -0,0 +1,21 @@
+import { redirect } from "next/navigation";
+
+import { createClient } from "@/lib/supabase/server";
+
+export default async function BlueprintProjectsLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    redirect("/auth");
+  }
+
+  return children;
+}
